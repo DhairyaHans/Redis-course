1fad0aea88a87214d0b1307dbad3bdfedb27f871
Dhairya Hans <dhairyahans2001@gmail.com>
2024-11-28 01:30:26 +0530

    Added Items

--- rbay/rbay/src/services/keys.ts
+++ rbay/rbay/src/services/keys.ts
@@ -1,3 +1,4 @@
 export const pageCacheKey = (id: String) => `pagecache#${id}`
 export const usersKey = (userId: String) => `users#${userId}`
-export const sessionsKey = (sessionId: String) => `sessions${sessionId}`
+export const sessionsKey = (sessionId: String) => `sessions#${sessionId}`
+export const itemsKey = (itemId: String) => `items#${itemId}`
--- rbay/rbay/src/services/queries/items/serialize.ts
+++ rbay/rbay/src/services/queries/items/serialize.ts
@@ -1,3 +1,10 @@
 import type { CreateItemAttrs } from '$services/types';
 
-export const serialize = (attrs: CreateItemAttrs) => {};
+export const serialize = (attrs: CreateItemAttrs, userId: string) => {
+    return {
+        ...attrs,
+        createdAt: attrs.createdAt.toMillis(),
+        endingAt: attrs.endingAt.toMillis(),
+        userId: userId
+    }
+};
