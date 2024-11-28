9b950a06420cc4285a9d7a7bcf8d60d2dd7aa35d
Dhairya Hans <dhairyahans2001@gmail.com>
2024-11-29 00:06:40 +0530

    Sets and pipelines

--- rbay/rbay/sandbox/index.ts
+++ rbay/rbay/sandbox/index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
+    /*
     await client.hSet("car", {
         color: "red",
         build: 1950
@@ -15,5 +16,36 @@ const run = async () => {
     }
 
     console.log(car)
+    */
+
+    // BUILDING PIPELINE
+
+    await client.hSet("car1", {
+        color: "red",
+        build: 1950
+    })
+    await client.hSet("car2", {
+        color: "green",
+        build: 1955
+    })
+    await client.hSet("car3", {
+        color: "blue",
+        build: 1960
+    })
+
+    const commands = [1,2,3].map((id) => {
+        return client.hGetAll("car" + id)
+    })
+    const results = await Promise.all(
+        commands
+    )
+
+    // const results = await Promise.all([
+    //     client.hGetAll("car1"),
+    //     client.hGetAll("car2"),
+    //     client.hGetAll("car3")
+    // ])
+
+    console.log(results)
 };
 run();
